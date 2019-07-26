6dfd42f8de61a2e773493ecdba4dc16f671f4d55
Marina Baskova <marinabaskova24@gmail.com>
2019-07-26 11:33:45 -0700

    added stretch for names

--- names/names.py
+++ names/names.py
@@ -10,23 +10,39 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 # v0
-duplicates = []
+# duplicates = []
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
 # v1
-memo = {}
+# memo = {}
 
-for name in names_1:
-    memo[name] = name
-for name in names_2:
-    if name in memo:
-        duplicates.append(name)
+# for name in names_1:
+#     memo[name] = name
+# for name in names_2:
+#     if name in memo:
+#         duplicates.append(name)
 
 # v2
 # duplicates = list(set(names_1) & set(names_2))
+
+
+# v3
+
+# names_1 = ["a", "b", "c"]
+# names_2 = ["a", "b", "e"]
+
+# v4 Stretch
+for i in range(len(names_1)):
+    # if the el exist in list 2
+    is_unique_element = names_1[i] not in names_2
+    if is_unique_element:
+        names_1[i] = None
+
+
+duplicates = [name for name in names_1 if name is not None]
 
 
 end_time = time.time()
@@ -36,3 +52,4 @@ print(f"runtime: {end_time - start_time} seconds")
 # runtime: 4.7013099193573 seconds - Original
 # runtime: 0.0035758018493652344 seconds - Using memo
 # runtime: 0.002888917922973633 seconds - Using set intersection
+# runtime: 1.028311014175415 seconds  - Stretch
