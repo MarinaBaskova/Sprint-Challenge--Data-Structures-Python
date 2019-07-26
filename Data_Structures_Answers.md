bb7e830bb3a90ff5f3e04f8473939bee42d15533
Marina Baskova <marinabaskova24@gmail.com>
2019-07-26 10:57:35 -0700

    added names

--- Data_Structures_Answers.md
+++ Data_Structures_Answers.md
@@ -9,9 +9,12 @@ Add your answers to the questions below.
 4. What is the space complexity of your ring buffer's `get` method?
    O(n)
 5. What is the runtime complexity of the provided code in `names.py`?
-
+   O(n^2)
 6. What is the space complexity of the provided code in `names.py`?
-
+   O(n)
 7. What is the runtime complexity of your optimized code in `names.py`?
-
+   Using memo : O(n).
+   Set intersection: O(n)
 8. What is the space complexity of your optimized code in `names.py`?
+   Using memo : O(n)
+   Set intersection: O(n)
--- names/names.py
+++ names/names.py
@@ -9,13 +9,14 @@ f.close()
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
-
+# v0
 duplicates = []
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
+# v1
 memo = {}
 
 for name in names_1:
@@ -24,9 +25,14 @@ for name in names_2:
     if name in memo:
         duplicates.append(name)
 
+# v2
+# duplicates = list(set(names_1) & set(names_2))
+
+
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
 
 # runtime: 4.7013099193573 seconds - Original
 # runtime: 0.0035758018493652344 seconds - Using memo
+# runtime: 0.002888917922973633 seconds - Using set intersection
