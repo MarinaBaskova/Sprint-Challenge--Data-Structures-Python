dd512f45187643531be43fc3387a825d45cd7910
Marina Baskova <marinabaskova24@gmail.com>
2019-07-26 09:38:06 -0700

    added names memo

--- names/names.py
+++ names/names.py
@@ -11,12 +11,22 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+memo = {}
+
+for name in names_1:
+    memo[name] = name
+for name in names_2:
+    if name in memo:
+        duplicates.append(name)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
+# runtime: 4.7013099193573 seconds - Original
+# runtime: 0.0035758018493652344 seconds - Using memo
